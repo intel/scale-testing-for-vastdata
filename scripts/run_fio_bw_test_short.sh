8ad8701b4ce04d2134b4778993b3f3650ab0217b
Chunhong Mao <chunhong.mao@intel.com>
2022-01-07 09:34:56 -0800

    Added 10GbE stepping test

--- scripts/run_fio_bw_test_short.sh
+++ scripts/run_fio_bw_test_short.sh
@@ -6,10 +6,19 @@
 script_dir="$(cd "$(dirname "$0")" || exit; pwd)"
 source "$script_dir/common.conf"
 
-"$SCRIPT_DIR"/run_test.sh -ns 100 -sf "$WORKLOAD_DIR"/fio/bw_test_short_100g.sch -co
+# Ensure it starts cleanly
+"$SCRIPT_DIR"/purge_cluster.sh
 
-# Run it 3 times
+# Create the files on 100GbE and run it 3 times, cleanup afterwards
+"$SCRIPT_DIR"/run_test.sh -ns 100 -sf "$WORKLOAD_DIR"/fio/bw_test_short_100g.sch -co
 "$SCRIPT_DIR"/run_test.sh -ns 100 -sf "$WORKLOAD_DIR"/fio/bw_test_short_100g.sch -ss 2 -pf run1
 "$SCRIPT_DIR"/run_test.sh -ns 100 -sf "$WORKLOAD_DIR"/fio/bw_test_short_100g.sch -ss 2 -pf run2
 "$SCRIPT_DIR"/run_test.sh -ns 100 -sf "$WORKLOAD_DIR"/fio/bw_test_short_100g.sch -ss 2 -pf run3
+"$SCRIPT_DIR"/purge_cluster.sh
+
+# Create the files on 10GbE and run it 3 times, cleanup afterwards
+"$SCRIPT_DIR"/run_test.sh -ns 10 -sf "$WORKLOAD_DIR"/fio/bw_test_short_10g.sch -co
+"$SCRIPT_DIR"/run_test.sh -ns 10 -sf "$WORKLOAD_DIR"/fio/bw_test_short_10g.sch -ss 10 -pf run1
+"$SCRIPT_DIR"/run_test.sh -ns 10 -sf "$WORKLOAD_DIR"/fio/bw_test_short_10g.sch -ss 10 -pf run2
+"$SCRIPT_DIR"/run_test.sh -ns 10 -sf "$WORKLOAD_DIR"/fio/bw_test_short_10g.sch -ss 10 -pf run3
 "$SCRIPT_DIR"/purge_cluster.sh
