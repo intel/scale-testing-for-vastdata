a3bf9ac996062de2f04f13eaa18854c8c9d918e1
Chunhong Mao <chunhong.mao@intel.com>
2022-01-07 10:44:49 -0800

    Added delay after purge for 10GbE stepping test

--- scripts/run_fio_bw_test_short.sh
+++ scripts/run_fio_bw_test_short.sh
@@ -6,15 +6,13 @@
 script_dir="$(cd "$(dirname "$0")" || exit; pwd)"
 source "$script_dir/common.conf"
 
-# Ensure it starts cleanly
-"$SCRIPT_DIR"/purge_cluster.sh
-
 # Create the files on 100GbE and run it 3 times, cleanup afterwards
 "$SCRIPT_DIR"/run_test.sh -ns 100 -sf "$WORKLOAD_DIR"/fio/bw_test_short_100g.sch -co
 "$SCRIPT_DIR"/run_test.sh -ns 100 -sf "$WORKLOAD_DIR"/fio/bw_test_short_100g.sch -ss 2 -pf run1
 "$SCRIPT_DIR"/run_test.sh -ns 100 -sf "$WORKLOAD_DIR"/fio/bw_test_short_100g.sch -ss 2 -pf run2
 "$SCRIPT_DIR"/run_test.sh -ns 100 -sf "$WORKLOAD_DIR"/fio/bw_test_short_100g.sch -ss 2 -pf run3
 "$SCRIPT_DIR"/purge_cluster.sh
+sleep 600
 
 # Create the files on 10GbE and run it 3 times, cleanup afterwards
 "$SCRIPT_DIR"/run_test.sh -ns 10 -sf "$WORKLOAD_DIR"/fio/bw_test_short_10g.sch -co
